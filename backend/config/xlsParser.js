41c448e6e87537888f08309c74cc4a31f135fc28
doc-han <yahyafarhan48@gmail.com>
2020-06-26 17:11:38 +0000

    parsed uploaded file

--- backend/config/xlsParser.js
+++ backend/config/xlsParser.js
@@ -3,8 +3,31 @@ class xlsParser {
     constructor(path) {
         this.nodeXls = require('node-xlsx');
         this.fs = require('fs');
+        this._ = require('lodash');
         this.path = path
         this.sheets = this.nodeXls.parse(this.fs.readFileSync(this.path));
+        this.keys;
+    }
+
+    refineSheets = () => {
+        // console.log(this.sheets)
+        let newsheets = [];
+        this.sheets.forEach(sheet => {
+            let rows = sheet.data;
+            this.keys = rows.splice(0, 1)[0];
+            this.keys = this.keys.map(key => this._.camelCase(key));
+            let newsheet = [];
+            rows.forEach(row => {
+                if(!row.length) return;
+                let newRow = {};
+                for (let i = 0; i < row.length; i++) {
+                    newRow[keys[i]] = row[i];
+                }
+                newsheet.push(newRow);
+            })
+            newsheets.push(newsheet);
+        })
+        this.sheets = newsheets;
     }
 
     getSheet = () => this.sheets;
--- backend/controllers/main.controller.js
+++ backend/controllers/main.controller.js
@@ -3,6 +3,7 @@ const xlsParser = require('../config/xlsParser');
 exports.upload = (req, res) => {
     // file: contains the uploaded file
     let { file } = req;
-    let a = xlsParser.init(file.path);
-    res.send(a.getSheet());
+    let xls = xlsParser.init(file.path);
+    xls.refineSheets();
+    res.send(xls.getSheet());
 }
