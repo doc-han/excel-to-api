3be24c83f0504fb4d3be7304acd5fc9b785d2cdf
doc-han <yahyafarhan48@gmail.com>
2020-06-28 23:42:23 +0000

    connecting mongodb

--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,20 @@ const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 // configurations
 const app = express();
 const PORT = process.env.PORT || 8080;
 dotenv.config();
+
+mongoose.connect(process.env.dbURI,{
+	useNewUrlParser: true
+});
+const db = mongoose.connection;
+db.on('error',function(err){
+	console.log(err);
+})
 
 // middlewares
 app.use(bodyParser.urlencoded({extended: false}));
